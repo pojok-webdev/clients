2dcfa77d2c5d6b16b6b7cd6e83a574585d67913d
pujie <pw.prayitno@gmail.com>
2019-02-17 14:06:12 +0700

    get color on load

--- src/app/clients/clients.page.ts
+++ src/app/clients/clients.page.ts
@@ -13,9 +13,19 @@ export class ClientsPage implements OnInit {
       this.clients = result
     })
   }
+  getButtonColor(clientCategory,buttonType){
+    console.log("clientCategory:",clientCategory)
+    if(clientCategory==buttonType){
+      return "danger"
+    }
+    return "primary"
+  }
   updateCategory(id,category){
+    let _this = this 
+    console.log("id",id)
     this.client.update({id:id,clientcategory:category},result=>{
       console.log("update category",result)
+     // _this.getButtonColor(category,category)
     })
   }
   ngOnInit() {
